6aee50f98fef50bc87a14256550d6612b3a93e87
AleksK <avkhatskevich@gmail.com>
2015-03-17 00:00:21 +0300

    Revert "Fixed: bug with deleting source files when cleaning"
    This reverts commit 4c7505be0249912e5dccfe1ff951fabc5338e02c.

--- Archivator/test/log_test.c
+++ Archivator/test/log_test.c
@@ -8,7 +8,7 @@ int main(){
     LOGMESG(LOG_ALL,"%d, %d\n", 5, 84);
     LOGMESG(LOG_FATAL,"Valera!!! %d,\n", 5);
     logClose();
-    logInit(LOG_INFO, "log_test_out_file_1_Valera" );
+    logInit(LOG_INFO, "log_test_file_1_Valera" );
     LOGMESG(LOG_INFO,"%d, %d\n", 5, 74);
     LOGMESG(LOG_ALL,"%d, %d\n", 5, 84);
     LOGMESG(LOG_FATAL,"Valera!!! %d,\n", 5);
